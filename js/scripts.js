c192ba1cad0cade7acb11a660978c8853c42ef4a
Bartosz Czarnota <bartek.bootcamp@gmail.com>
2017-03-30 19:15:24 +0200

    Create third project i JS

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// plik scripts.js
+
+var a = prompt('Wpisz długość podstawy a:');
+var h = prompt('Wpisz wysokość h:');
+var triangleArea = ( (a * h) /2 );
+alert('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+// alert('Wpisz wysokość h:, ' + h);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+// poniżej zakomentowałem wywołanie funkcji, aby ją wyłączyć
+// console.log(‘Zakomentowana się nie wywoła’);
+
+// var name = prompt('Sign Your name');
+// alert('Hello, ' + name);
+// console.log('Hello, ' + name);
